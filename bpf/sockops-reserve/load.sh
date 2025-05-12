e3379e581e6b4c237035729a6a06c488dbe6d888
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2025-05-12 22:26:47 +0330

    Debug

--- bpf/sockops-reserve/load.sh
+++ bpf/sockops-reserve/load.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 clang -g -O2 -target bpf -c reserve.bpf.c -o reserve.bpf.o
-sudo bpftool prog load /sdf/P4-TCP-INT/bpf/sockops-reserve/reserve.bpf.o "/sys/fs/bpf/bpf_sockop"
+sudo bpftool prog load reserve.bpf.o "/sys/fs/bpf/bpf_sockop"
 sudo bpftool cgroup attach "/sys/fs/cgroup/unified/dash_server.slice/unified.scope/" sock_ops pinned "/sys/fs/bpf/bpf_sockop"
