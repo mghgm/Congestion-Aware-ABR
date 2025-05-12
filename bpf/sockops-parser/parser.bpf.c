9ca25bfdcbec65cca4b653697605a078f1cdd00f
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2025-05-13 00:42:56 +0330

    Debug parser

--- bpf/sockops-parser/parser.bpf.c
+++ bpf/sockops-parser/parser.bpf.c
@@ -11,7 +11,7 @@
 
 
 struct __attribute__((packed)) int_option {
-    __u64 Timestamp: 64;
+    __u64 timestamp: 64;
     __u8  queue_depth: 8;
     __u16 timedelta: 16;
 };
@@ -20,7 +20,7 @@ struct __attribute__((packed)) int_option {
 struct {
     __uint(type, BPF_MAP_TYPE_QUEUE);
     __uint(max_entries, QUEUE_MAX_ENTRIES);
-    __uint(value_size, sizeof(parsed_opt));
+    __uint(value_size, sizeof(struct int_option));
     __uint(key_size, 0);
 
 } int_map SEC(".maps");
@@ -50,12 +50,10 @@ int bpf_sockops_parse_tcp_options(struct bpf_sock_ops *skops) {
 
     	    struct int_option parsed_opt = {0};
             parsed_opt.queue_depth = opt_buf[8];
-            parsed_opt.timedelta = *((__u16 *) &opt_buf[11]);
+    	    parsed_opt.timedelta = (opt_buf[10] << 8) + opt_buf[11];
+    	    parsed_opt.timestamp = bpf_ktime_get_boot_ns();
 
-            // TODO: Put data into parsed_option from buf
-            
     	    bpf_printk("found! enq_depth = %d timestamp = %lld\n", opt_buf[8] , bpf_ktime_get_boot_ns());
-            bpf_printk("parsed! queue_depth = %d timedelta = %d", parsed_opt.queue_depth, parsed_opt.timedelta);
 
             int rv;
             rv = bpf_map_push_elem(&int_map, &parsed_opt, 0);
